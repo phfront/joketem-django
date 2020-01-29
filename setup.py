e5596f9b7d9bf480b1821083c99d8e4745ef8262
pedro <pedro.phdois@gmail.com>
2020-01-29 17:42:56 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='Joketem Django',
+    version='1.0',
+    description='',
+    author='Pedro Henrique Silva Rosa',
+    author_email='pedro.phdois@gmail.com',
+    url=''
+)
